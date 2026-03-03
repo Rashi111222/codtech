fba3d675c470dc96c994042035b6753e222cfdd0
Rashi Karande <rashikarande2214@gmail.com>
2026-03-03 21:25:56 +0530

    Implemented read functionality

--- Task1_FileUtility/FileOperations.java
+++ Task1_FileUtility/FileOperations.java
@@ -1,12 +1,15 @@
-package Task1_FileUtility;
+
 import java.io.*;
 
 public class FileOperations {
     public static void main(String args[]){
         String fileName="sample.txt";
         writeFile(fileName,"Hello!\nWelcome to Java Handling!");
+
+        readFile(fileName);
     }
 
+    /*Write Function */
     public static void writeFile(String fileName,String content){
         try{
             FileWriter writer= new FileWriter(fileName);
@@ -16,6 +19,23 @@ public class FileOperations {
         }
         catch(IOException e){
             System.out.println("Error while writing ");
+        }
+    }
+
+    /*Read function */
+    public static void readFile(String fileName){
+        try{
+            BufferedReader reader=new BufferedReader(new FileReader(fileName));
+            String line;
+
+            System.out.println("Reading file contentt: ");
+
+            while((line=reader.readLine())!=null){
+                System.out.println(line);
+            }
+        }
+        catch(IOException e){
+            System.out.println("Error while reading file");
         }
     }
 }
