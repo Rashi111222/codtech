e17138e88670e46ab05636ccbcf0ecca4a55f193
Rashi Karande <rashikarande2214@gmail.com>
2026-03-03 21:13:37 +0530

    Implemented writeFile functionality

--- Task1_FileUtility/FileOperations.java
+++ Task1_FileUtility/FileOperations.java
@@ -1,7 +1,21 @@
 package Task1_FileUtility;
+import java.io.*;
 
 public class FileOperations {
     public static void main(String args[]){
-        System.out.println("Task 1 File Utility");
+        String fileName="sample.txt";
+        writeFile(fileName,"Hello!\nWelcome to Java Handling!");
+    }
+
+    public static void writeFile(String fileName,String content){
+        try{
+            FileWriter writer= new FileWriter(fileName);
+            writer.write(content);
+            writer.close();
+            System.out.println("File written successfully");
+        }
+        catch(IOException e){
+            System.out.println("Error while writing ");
+        }
     }
 }
