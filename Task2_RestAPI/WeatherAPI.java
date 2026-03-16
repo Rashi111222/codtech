b391c1022f941421d4410850242d4f7c5add0ff2
Rashi Karande <rashikarande2214@gmail.com>
2026-03-16 23:56:24 +0530

    Added Api key and created url

--- Task2_RestAPI/WeatherAPI.java
+++ Task2_RestAPI/WeatherAPI.java
@@ -5,6 +5,14 @@ import java.net.*;
 public class WeatherAPI {
     public static void main(String args[])
     {
-        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter city name: ");
+        String city=sc.nextLine();
+
+        String apiKey="3af5c95b01bd893312e8fa1f83ae712a";
+
+        String urlString= "https://api.openweathermap.org/data/2.5/weather?q="
+            + city + "&appid=" + apiKey + "&units=metric";
+
     }
 }
